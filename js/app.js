d3c1e41db24dd39e4913f1e97ec99b35ab0b766f
Lizzie Paris <lzz.paris@gmail.com>
2016-04-11 21:57:34 -0700

    text input adds to stuff-i-need__list; other minor changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
 
+	$(".add-item").on("click",".add-item__btn",function(event){
+		event.preventDefault();
+		var itemToAdd = $(this).parent().children(".add-item__input").val();
+		$(".stuff-i-need__list").prepend("<li class=\"sl-item\">"+itemToAdd+"</li>");
+		$(this).parent().children(".add-item__input").val("");
+	});
 
+	$(".sl-item").mouseenter(function(){
+		$(this).css("border-color","#444");
+		// $(this).before().css("display","inline");
+
+	})
+	.mouseleave(function(){
+		$(this).css("border-color","white");
+		// $("this:before").css("display","none");		
+	});
  
-}
+});
