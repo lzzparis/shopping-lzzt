eeab27eaf1eb560b72a6dfc628a7b1d0f3ab3ed8
Lizzie Paris <lzz.paris@gmail.com>
2016-06-30 23:11:27 -0700

    refactor js into named functions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
 
-	$(".add-item").on("click",".add-item__btn",function(event){
+	$(".add-item").on("click",".add-item__btn",addItem);
+
+	$(".stuff-i-need__list").on("click",".sl-item > .check",purchaseItem)
+	.on("mouseenter",".sl-item",blackenIcon);
+
+
+	$(".in-my-bag__list").on("click",".sl-item > .back-up",reinstateItem)
+	.on("mouseenter",".sl-item",darkenIcon);
+
+
+	$(".list").on("click",".sl-item > .ex", deleteItem)
+	.on("mouseleave",".sl-item",whitenIcon);
+
+});
+
+function addItem(event){
 		event.preventDefault();
 		var itemToAdd = $(this).parent().children(".add-item__input").val();
 		if (itemToAdd != ""){
@@ -13,56 +28,45 @@ $(document).ready(function(){
 		else {
 			alert("Please type in the box to add items");
 		}
-	});
+	}
 
-	$(".stuff-i-need__list").on("click",".sl-item > .check",function(){
+function purchaseItem(){
 		purchasedItem = $(this).parent(".sl-item");
 		purchaedItemName = purchasedItem.children(".middle").html();
 		$(".in-my-bag__list").append(
 			"<li class=\"sl-item\"><span class=\"side back-up\">&#x21e1;</span><span class=\"middle\">"
 			+purchaedItemName+"</span><span class=\"side ex\">&#x2717;</span></li>");
 		purchasedItem.remove();
-	}).on("mouseenter",".sl-item",function(){
-		$(this).children(".side").css("color","black");
+	}
 
-
-	});
-
-
-	$(".in-my-bag__list").on("click",".sl-item > .back-up",function(){
-		reinstatedItem = $(this).parent(".sl-item");
-		reinstatedItemName = reinstatedItem.children(".middle").html();
-		$(".stuff-i-need__list").prepend(
-			"<li class=\"sl-item\"><span class=\"side check\">&#x2713;</span><span class=\"middle\">"
-			+reinstatedItemName+"</span><span class=\"side ex\">&#x2717;</span></li>");
-		reinstatedItem.remove();
-	}).on("mouseenter",".sl-item",function(){
-		$(this).children(".side").css("color","#888");
-
-
-	});
-
-
-	$(".list")
-	.on("mouseleave",".sl-item",function(){
-		$(this).children(".side").css("color","white");
-
-	})
-	.on("click",".sl-item > .ex", function(){
+function reinstateItem(){
+	reinstatedItem = $(this).parent(".sl-item");
+	reinstatedItemName = reinstatedItem.children(".middle").html();
+	$(".stuff-i-need__list").prepend(
+		"<li class=\"sl-item\"><span class=\"side check\">&#x2713;</span><span class=\"middle\">"
+		+reinstatedItemName+"</span><span class=\"side ex\">&#x2717;</span></li>");
+	reinstatedItem.remove();
+}
+function deleteItem() {
 		deletedItem = $(this).parent(".sl-item");
 		deletedItem.remove();
-	});
+}
+
+//todo
+// function colorIcon(color){
+// 		return {$(this).children(".side").css("color",color)};
+// }
+
+function blackenIcon(){
+		$(this).children(".side").css("color","black");
+}
+
+function darkenIcon(){
+		$(this).children(".side").css("color","#888");
+}
 
 
+function whitenIcon() {
+		$(this).children(".side").css("color","white");
+}
 
- 
-});
-
-
-//TODO:
-//
-//update the prepends
-//hover behavior for check/x
-//proper symbol for check/x
-//make sure all listeners are already on the page
-//"remove" behavior
